0b5953af4c35411f23a5ef1fce7b960808ff1a1d
김영은 <jessicak0828@gmail.com>
2023-12-30 16:46:15 +0900

    < generateStaticParams > make static pages at dynamic routing

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -9,3 +9,10 @@ type Props = {
 export default function ProductsPage({ params }: Props) {
 	return <div>{params.slug}Page</div>;
 }
+
+export function generateStaticParams() {
+	const products = ['pants', 'skirts'];
+	return products.map((product) => ({
+		slug: product,
+	}));
+}
