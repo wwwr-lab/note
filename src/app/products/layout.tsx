b461562f1d755006937fb96f7315d85251231eb0
김영은 <jessicak0828@gmail.com>
2023-12-31 01:49:12 +0900

    <Metadata> create metadata for each page

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,6 +1,13 @@
 import React, { PropsWithChildren } from 'react';
 import styles from './layout.module.css';
 import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: '멋진 제품 사이트 | 전체 제품 확인',
+	description: '멋진 제품을 확인해보세요.',
+};
+
 interface Props extends PropsWithChildren {}
 export default function ProductsLayout({ children }: Props) {
 	return (
