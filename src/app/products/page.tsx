c2c087cbf4a2e5f4c63cd040db6f8acc85b8eabf
김영은 <jessicak0828@gmail.com>
2023-12-31 21:39:57 +0900

    < CSR > - use promise to use useState in useEffect

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 import { getProducts } from '@/service/products';
-import styles from './layout.module.css';
-export const revalidate = 3;
+import MeowArticle from '../components/MeowArticle';
+
+//export const revalidate = 3;
 export default async function ProductsPage() {
 	const products = await getProducts();
-	const res = await fetch('http://meowherokuapp.com', {
-		next: { revalidate: 0 }, //	cache: 'force-cache', //SSR
-	});
-	const data = res.json();
-	const factText = data.data[0];
+
 	return (
 		<div>
 			<h1>ProductsPage</h1>
@@ -21,7 +18,7 @@ export default async function ProductsPage() {
 						</li>
 					))}
 				</ul>
-				<article className={styles.article}> {factText}</article>
+			<MeowArticle/>
 			</nav>
 		</div>
 	);
