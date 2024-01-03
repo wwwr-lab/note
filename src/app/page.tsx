a1f81822f3329a1035143c0f5f0c6acc275a265e
김영은 <jessicak0828@gmail.com>
2024-01-03 23:05:26 +0900

    importing image from the online instead of server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-   
-   <h1>HomePage</h1>
-  )
+	return (
+		<>
+			<h1>HomePage</h1>
+			<Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" width={500} height={500} alt="Store" />
+		</>
+	);
 }
