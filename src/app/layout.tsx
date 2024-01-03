e1da200dbf91653c57d39e20bd6ba18ea87c8316
김영은 <jessicak0828@gmail.com>
2024-01-04 00:02:20 +0900

    using Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
-import { Inter } from 'next/font/google';
+
+import { Open_Sans, Nanum_Gothic } from 'next/font/google';
 import './globals.css';
 import styles from './layout.module.css';
-const inter = Inter({ subsets: ['latin'] });
+
+const sans = Open_Sans({ subsets: ['latin'] });
+const gothic = Nanum_Gothic({ weight: '700', subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: '멋진 제품 사이트',
 	description: '멋진 제품을 판매하는 곳입니다.',
-  icons : {
-    icon: '/favicon.ico'
-  }
+	icons: {
+		icon: '/favicon.ico',
+	},
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>
+		<html lang="ko">
+			<body className={sans.className}>
 				<header className={styles.header}>
-					<h1>Demo Note</h1>
+					<h1 className={gothic.className}>Demo Note</h1>
 					<nav className={styles.nav}>
 						<Link href="/products">Products</Link>
 						<Link href="/contact">Contact</Link>
 						<Link href="/about">About</Link>
-
 					</nav>
 				</header>
 				{children}
