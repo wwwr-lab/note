621cac0bf45106e1fa6ad4fcaf201bc529b41ff8
김영은 <jessicak0828@gmail.com>
2024-01-04 04:44:55 +0900

    ver2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
 	return (
 		<>
-			<h1>HomePage</h1>
+			<h1>HomePage ver2</h1>
 			<Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" width={500} height={500} alt="Store" />
 		</>
 	);
